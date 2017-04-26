a35e292549ad881fff4d98c3485a97df05a53c4e
Hakan Yildizay [Mobil Yazilim  Servisi] <Hakan.Yildizay@ingbank.com.tr>
2017-04-26 23:32:46 +0300

    Initial Commit

---
+++ INGHackthon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  INGHackthon
+//
+//  Created by Hakan Yildizay [Mobil Yazilim  Servisi] on 26/04/2017.
+//  Copyright © 2017 ING BANK. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
