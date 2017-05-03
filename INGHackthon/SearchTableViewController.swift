3d1dbdf45a3d4688072de273f42535339d3a5807
Hakan Yildizay [Mobil Yazilim  Servisi] <Hakan.Yildizay@ingbank.com.tr>
2017-05-04 00:20:04 +0300

    Adding Search ViewController

--- INGHackthon/SearchTableViewController.swift
+++ INGHackthon/SearchTableViewController.swift
@@ -26,7 +26,7 @@ class SearchTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+/*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
@@ -36,7 +36,7 @@ class SearchTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
+*/
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
@@ -46,6 +46,53 @@ class SearchTableViewController: UITableViewController {
         return cell
     }
     */
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50.0
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 75.0, height: 30.0))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Change", for: UIControlState.normal)
+        button.setTitleColor(UIColor.orange, for: UIControlState.normal)
+        button.sizeToFit()
+        
+        
+        let sectionHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: self.tableView.frame.width, height: 30.0))
+        sectionHeaderView.backgroundColor = UIColor.clear
+        let label = UILabel(frame: sectionHeaderView.frame)
+        label.text = "Search locatiopn: TURKEY"
+        label.textColor = UIColor(red: 0.0, green: 57.0/155.0, blue: 118.0/255.0, alpha: 1.0)
+        label.font = UIFont.init(name: "Helvetica-Neue", size: 16.0)
+        label.sizeToFit()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        sectionHeaderView.addSubview(label)
+        
+        sectionHeaderView.addSubview(button)
+        
+        let verticalConstraint = NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: sectionHeaderView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+        sectionHeaderView.addConstraint(verticalConstraint)
+        
+        let leftConstraint = NSLayoutConstraint(item: label, attribute: .leading, relatedBy: .equal, toItem: sectionHeaderView, attribute: .leading, multiplier: 1.0, constant: 14)
+        sectionHeaderView.addConstraint(leftConstraint)
+        
+        
+        let rightConstraint = NSLayoutConstraint(item: button, attribute: .trailing, relatedBy: .equal, toItem: sectionHeaderView, attribute: .trailing, multiplier: 1.0, constant: -10.0)
+        
+        sectionHeaderView.addConstraint(rightConstraint)
+        
+        let buttonVertAlignment =  NSLayoutConstraint(item: button, attribute: .centerY, relatedBy: .equal, toItem: sectionHeaderView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+        sectionHeaderView.addConstraint(buttonVertAlignment)
+        
+        
+        sectionHeaderView.setNeedsUpdateConstraints()
+        sectionHeaderView.setNeedsLayout()
+        
+        return sectionHeaderView
+        
+    }
 
     /*
     // Override to support conditional editing of the table view.
